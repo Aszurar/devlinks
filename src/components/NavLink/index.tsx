082dbcf38a4d4c6678f371d169be1001de42df8d
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-11-09 12:39:51 -0300

    :sparkles: Criação dos botões dos projetos :sparkles:

---
+++ src/components/NavLink/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "react"
+
+type NavLinkProps = ComponentProps<"a">
+
+export function NavLink({ children, ...rest }: NavLinkProps) {
+  return (
+    <a {...rest}
+      className="h-14 rounded-lg bg-surface-normal flex items-center justify-center w-full text-center px-4 hover:bg-surface-strong border border-stroke hover:border-0.375 hover:border-white cursor-pointer transition-0.1"
+    >
+      <span className="text-base text-white">
+        {children}
+      </span>
+    </a>
+
+  )
+}
