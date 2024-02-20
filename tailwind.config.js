0e71a57f6985134d270fb38747848ea0d12cbbf8
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-02-20 19:56:31 -0300

    💄 Adiciona configuração de dark mode e atualiza cores e estilos de fundo 💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import plugin from 'tailwindcss/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
@@ -10,11 +11,23 @@ export default {
       },
 
       colors: {
-        highlight: 'rgba(255, 255, 255, 0.2)',
-        stroke: 'rgba(255, 255, 255, 0.5)',
+        highlight: {
+          light: 'rgba(0, 0, 0, 0.1)',
+          dark: 'rgba(255, 255, 255, 0.2)',
+        },
+        stroke: {
+          dark: 'rgba(255, 255, 255, 0.5)',
+          light: 'rgba(0, 0, 0, 0.5)',
+        },
         surface: {
-          normal: 'rgba(255, 255, 255, 0.1)',
-          strong: 'rgba(255, 255, 255, 0.05)',
+          dark: 'rgba(255, 255, 255, 0.1)',
+          light: 'rgba(0, 0, 0, 0.05)',
+          'hover-light': 'rgba(0, 0, 0, 0.01)',
+          'hover-dark': 'rgba(255, 255, 255, 0.05)',
+        },
+        text: {
+          light: 'rgba(255, 255, 255, 255)',
+          dark: 'rgba(0, 0, 0, 0)',
         },
       },
     },
@@ -25,9 +38,31 @@ export default {
         '.transition-200ms': {
           transition: 'all 0.2s ease',
         },
-
-        '.background-full': {
-          'background-image': "url('./src/assets/imgs/background.png')",
+        '.animate-opacity': {
+          'animation-timing-function': 'cubic-bezier(0.16, 1, 0.3, 1)',
+          'will-change': 'transform, opacity',
+        },
+        '.background-dark-full': {
+          'background-image': "url('./src/assets/imgs/background-dark.png')",
+          'background-repeat': 'no-repeat',
+          'background-position': 'top center',
+          'background-size': 'cover',
+        },
+        '.background-light-full': {
+          'background-image': "url('./src/assets/imgs/background-light.png')",
+          'background-repeat': 'no-repeat',
+          'background-position': 'top center',
+          'background-size': 'cover',
+        },
+        '.background-mb-light-full': {
+          'background-image':
+            "url('./src/assets/imgs/background-mb-light.png')",
+          'background-repeat': 'no-repeat',
+          'background-position': 'top center',
+          'background-size': 'cover',
+        },
+        '.background-mb-dark-full': {
+          'background-image': "url('./src/assets/imgs/background-mb-dark.png')",
           'background-repeat': 'no-repeat',
           'background-position': 'top center',
           'background-size': 'cover',
