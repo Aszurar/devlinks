f642208dd34cd7dd32059ec5a5cc7fe12950645c
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-02-20 20:00:49 -0300

    ✨ Adiciona suporte a tooltips no componente IconButton ✨

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,4 +1,4 @@
-
+import * as Tooltip from '@radix-ui/react-tooltip'
 type IconButtonProps = {
   icon: string
   link: string
@@ -7,8 +7,36 @@ type IconButtonProps = {
 
 export function IconButton({ icon, link, label }: Readonly<IconButtonProps>) {
   return (
-    <a href={link} target="_blank" aria-label={label} className=" hover:bg-highlight transition-200ms h-10 w-10 flex items-center justify-center rounded-md cursor-pointer">
-      <img src={icon} alt="" className="h-6 w-6" />
-    </a>
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <a
+            href={link}
+            target="_blank"
+            aria-label={label}
+            rel="noreferrer"
+            className={`dark:hover:bg-highlight-dark hover:bg-highlight-light flex h-10 
+            w-10 cursor-pointer items-center 
+            justify-center rounded-full transition-200ms`}
+          >
+            <img src={icon} alt="" className="h-6 w-6" />
+          </a>
+        </Tooltip.Trigger>
+
+        <Tooltip.Portal>
+          <Tooltip.Content
+            side="bottom"
+            className={` animate-opacity box-shadow-tooltip 
+              rounded-md bg-white px-3 py-1.5 text-xs font-medium 
+               transition-all duration-[200ms]
+              dark:bg-surface-normal dark:text-white
+          `}
+          >
+            {label}
+            <Tooltip.Arrow className="dark:fill-surface-normal" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   )
 }
