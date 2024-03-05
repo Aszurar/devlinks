625f1fec4601cb95cf29aca5d0b4776a75320758
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-03-05 20:55:25 -0300

    ✨ Atualizar importação do componente Tooltip e refatorar código do IconButton e NavLink ✨

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,4 +1,5 @@
-import * as Tooltip from '@radix-ui/react-tooltip'
+import { Tooltip } from '../Tooltip'
+
 type IconButtonProps = {
   icon: string
   link: string
@@ -7,36 +8,18 @@ type IconButtonProps = {
 
 export function IconButton({ icon, link, label }: Readonly<IconButtonProps>) {
   return (
-    <Tooltip.Provider>
-      <Tooltip.Root>
-        <Tooltip.Trigger asChild>
-          <a
-            href={link}
-            target="_blank"
-            aria-label={label}
-            rel="noreferrer"
-            className={`dark:hover:bg-highlight-dark hover:bg-highlight-light flex h-10 
-            w-10 cursor-pointer items-center 
-            justify-center rounded-full transition-200ms`}
-          >
-            <img src={icon} alt="" className="h-6 w-6" />
-          </a>
-        </Tooltip.Trigger>
-
-        <Tooltip.Portal>
-          <Tooltip.Content
-            side="bottom"
-            className={` animate-opacity box-shadow-tooltip 
-              rounded-md bg-white px-3 py-1.5 text-xs font-medium 
-               transition-all duration-[200ms]
-              dark:bg-surface-normal dark:text-white
-          `}
-          >
-            {label}
-            <Tooltip.Arrow className="dark:fill-surface-normal" />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
+    <Tooltip label={label} asChild>
+      <a
+        href={link}
+        target="_blank"
+        aria-label={label}
+        rel="noreferrer"
+        className={`flex h-10 w-10 cursor-pointer 
+            items-center justify-center rounded-full 
+            transition-200ms hover:bg-highlight-light dark:hover:bg-highlight-dark`}
+      >
+        <img src={icon} alt="" className="h-6 w-6" />
+      </a>
+    </Tooltip>
   )
 }
--- src/components/NavLink/index.tsx
+++ src/components/NavLink/index.tsx
@@ -6,13 +6,12 @@ export function NavLink({ children, ...rest }: NavLinkProps) {
   return (
     <a
       target="_blank"
-      className={`dark:border-stroke-dark dark:bg-surface-dark 
-        border-stroke-light bg-surface-light 
-        hover:bg-surface-hover-light dark:hover:bg-surface-hover-dark flex h-14 w-full 
-        cursor-pointer items-center justify-center gap-2  rounded-lg border 
-        bg-surface-normal  px-4 text-center transition-colors
-        duration-500 hover:border-0.375 hover:border-black
-        dark:hover:border-white
+      className={`bg-surface-normal flex h-14 w-full cursor-pointer items-center 
+        justify-center gap-2 rounded-lg border border-stroke-light 
+        bg-surface-light px-4  text-center transition-colors duration-500  
+        hover:border-0.375 hover:border-black hover:bg-surface-hover-light
+        dark:border-stroke-dark dark:bg-surface-dark dark:hover:border-white
+        dark:hover:bg-surface-hover-dark
         `}
       {...rest}
     >
